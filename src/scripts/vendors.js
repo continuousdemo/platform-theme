3e78f6b32b55fb3f383b9ccb6de6f44bbabb1e2e
Yannick Huard <huard.yan@gmail.com>
2016-03-02 17:39:20 +0100

    fix(vendors): correct imports

--- src/scripts/vendors.js
+++ src/scripts/vendors.js
@@ -1,1 +1,20 @@
-import 'ne-theme-framework-dev/src/framework/vendors';
+import 'ne-theme-framework-dev/src/framework/vendors/ie10';
+
+// jQuery and jQuery-once are already provided by Drupal
+// import 'jquery/dist/jquery';
+// import 'jquery-once/jquery.once';
+import 'matchHeight/dist/jquery.matchHeight';
+import 'ne-theme-framework-dev/src/framework/vendors/selectify';
+import 'jquery-ui/ui/core';
+import 'jquery-ui/ui/datepicker';
+
+// Bootstrap components
+import 'ne-theme-framework-dev/src/framework/vendors/bootstrap/scripts/bootstrap';
+
+// Modernizr
+import 'ne-theme-framework-dev/src/framework/vendors/modernizr-custom/modernizr-custom';
+
+// Other libraries
+import 'matchMedia/matchMedia';
+import 'enquire/dist/enquire';
+import 'velocity/velocity';
